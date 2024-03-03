6d7bbcfbc1b9f3c32096095450c8ce11b59d2fef
mohan <mohanrajvengi@gmail.com>
2024-03-03 13:06:51 +0530

    🐛 fix : video player rerender issue

--- src/components/video.player.jsx
+++ src/components/video.player.jsx
@@ -1,17 +1,21 @@
+import { memo } from "react";
 import PropTypes from "prop-types";
 
-export const VideoPlayer = ({ source }) => {
+function VideoPlayerComponent({ source }) {
   return (
     <video className="w-full md:h-6/7" controls muted playsInline autoPlay>
       <source src={source} type="video/mp4" />
     </video>
   );
-};
+}
 
-VideoPlayer.propTypes = {
+VideoPlayerComponent.propTypes = {
   source: PropTypes.string.isRequired,
 };
 
-VideoPlayer.defaultProps = {
+VideoPlayerComponent.defaultProps = {
   source: "",
 };
+
+
+export const VideoPlayer = memo(VideoPlayerComponent);
