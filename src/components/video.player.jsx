6288abf85524997c186a9857491e90876d99403d
mohan <mohanrajvengi@gmail.com>
2024-03-03 16:40:42 +0530

    🐛 fix : double click issue

--- src/components/video.player.jsx
+++ src/components/video.player.jsx
@@ -3,7 +3,7 @@ import PropTypes from "prop-types";
 
 function VideoPlayerComponent({ source }) {
   return (
-    <video className="w-full md:h-6/7" controls muted playsInline autoPlay>
+    <video className="w-full md:h-6/7" controls muted playsInline autoPlay >
       <source src={source} type="video/mp4" />
     </video>
   );
